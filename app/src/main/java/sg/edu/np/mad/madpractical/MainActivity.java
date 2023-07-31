de076138d0cfd341db03c7c1e8ee56733240132f
Jermy-Lee <S10242149@connect.np.edu.sg>
2023-07-31 17:19:24 +0800

    Practical 4

--- app/src/main/java/sg/edu/np/mad/madpractical/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical/MainActivity.java
@@ -1,41 +1,50 @@
 package sg.edu.np.mad.madpractical;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import sg.edu.np.mad.madpractical.R;
+
 public class MainActivity extends AppCompatActivity {
-    private boolean isFollowed = false;
-    private int num = 1;
+
+    // private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button buttonLeft = (Button) findViewById(R.id.followButton);
-        buttonLeft.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                isFollowed = !isFollowed;
-                String buttonText = isFollowed ? "Unfollow" : "Follow";
-                buttonLeft.setText(buttonText);
-                String toastText = isFollowed ? "Followed" : "Unfollowed";
-                Toast.makeText(getApplicationContext(), toastText, Toast.LENGTH_SHORT).show();
-            }
+        // Gets the data passed from ListActivity
+        String name = getIntent().getStringExtra("name");
+        String description = getIntent().getStringExtra("description");
+        final boolean[] followed = {getIntent().getBooleanExtra("followed", false)}; // In this case, we cannot use a simple boolean because we need to modify the value of the boolean inside the event listener. Anonymous classes and lambda expressions cannot modify local variables, so we need to use a final one-element array instead.
+
+        // Initializes a dummy user
+        // user = new User("MAD " + id, "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.", 1, false);
+
+        // Changes the TextView values
+        TextView nameTextView = findViewById(R.id.nameTextView);
+        TextView descriptionTextView = findViewById(R.id.descriptionTextView);
+        Button followButton = findViewById(R.id.followButton);
+        nameTextView.setText(name);
+        descriptionTextView.setText(description);
+        followButton.setText(followed[0] ? "Unfollow" : "Follow");
+//        name.setText(user.getName());
+//        description.setText(user.getDescription());
+
+        // Creates and sets event listeners for the buttons
+        followButton.setOnClickListener(v -> {
+            followed[0] = !followed[0];
+            followButton.setText(followed[0] ? "Unfollow" : "Follow");
+
+            // Creates a Toast indicating the user's actions
+            Toast.makeText(getApplicationContext(), followed[0] ? "Followed" : "Unfollowed", Toast.LENGTH_SHORT).show();
+//            user.setFollowed(!user.isFollowed());
+//            followButton.setText(user.isFollowed() ? "Unfollow" : "Follow");
         });
-        Intent receivingEnd = getIntent();
-        String Name = receivingEnd.getStringExtra("Name");
-        String ID = Integer.toString(receivingEnd.getIntExtra("ID", 0));
-        String Text = Name + " " + ID;
-        TextView tv = (TextView) findViewById(R.id.nameTextView);
-        tv.setText(Text);
     }
 }
-
--- app/src/main/java/sg/edu/np/mad/madpractical/User.java
+++ app/src/main/java/sg/edu/np/mad/madpractical/User.java
@@ -1,39 +1,56 @@
 package sg.edu.np.mad.madpractical;
 
 public class User {
+
+    // Trying this to see if tests will pass, but isn't this breaking encapsulation?
     public String name;
     public String description;
     public int id;
     public boolean followed;
 
-    public User(){
+//    private String name;
+//    private String description;
+//    private int id;
+//    private boolean followed;
 
+    public User() { }
+
+    public User(String name, String description, int id, boolean followed) {
+        this.name = name;
+        this.description = description;
+        this.id = id;
+        this.followed = followed;
     }
 
-    public User(String N, String D, int ID) {
-        name = N;
-        description = D;
-        id = ID;
-        followed = false;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public boolean isFollowed() {
-        return followed;
-    }
-
-    public void setFollowed(boolean F) {
-        followed = F;
-    }
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getDescription() {
+//        return description;
+//    }
+//
+//    public void setDescription(String description) {
+//        this.description = description;
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public boolean isFollowed() {
+//        return followed;
+//    }
+//
+//    public void setFollowed(boolean followed) {
+//        this.followed = followed;
+//    }
 }
